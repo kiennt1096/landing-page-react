1c0350db1a5f9d1838fac86e6adea51903c85891
Nguyen Trung Kien <kiennt3@rikkeisoft.com>
2023-08-14 21:59:14 +0900

    2:01:52 Footer

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Residencies from "./components/Residencies/Residencies";
 import Value from "./components/Value/Value";
 import Contacts from "./components/Contacts/Contacts";
 import GetStarted from "./components/GetStarted/GetStarted";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
       <Value />
       <Contacts />
       <GetStarted />
+      <Footer />
     </div>
   );
 }
